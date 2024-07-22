3a9b59da1955857b73c34671257fc6c816d51a8d
Alexis-Marion <alexis.marion@umontpellier.fr>
2024-07-22 19:21:26 +0200

    commit

---
+++ Analyses of the fossil record/Model_selection/run_Model_selection.sh
@@ -0,0 +1,3 @@
+# Compare wether two different set up of parameters influence rate estimation and Ts/Te values in PyRate
+
+Rscript Model_selection.r ../../PyRate_scripts/Dummy_data/pyrate_mcmc_logs/ ../../PyRate_scripts/Dummy_data_2/pyrate_mcmc_logs/ estimate.tsv
